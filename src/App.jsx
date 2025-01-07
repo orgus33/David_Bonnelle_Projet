bf4e4c97c1c12531bc9364ce7ab7407d93a9d9cd
Bastien <bastien.b200405@gmail.com>
2025-01-07 18:30:23 +0100

    Add boostrap

--- src/App.jsx
+++ src/App.jsx
@@ -1,96 +1,31 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css';
-import 'flowbite';
+
 
 function App() {
-  const [count, setCount] = useState(0)
 
   return (
       <>
-
-
-          <div className="p-4 mb-4 text-sm text-blue-800 rounded-lg bg-blue-50 dark:bg-gray-800 dark:text-blue-400"
-               role="alert">
-              <span className="font-medium">Info alert!</span> Change a few things up and try submitting again.
-          </div>
-          <div className="p-4 mb-4 text-sm text-red-800 rounded-lg bg-red-50 dark:bg-gray-800 dark:text-red-400"
-               role="alert">
-              <span className="font-medium">Danger alert!</span> Change a few things up and try submitting again.
-          </div>
-          <div className="p-4 mb-4 text-sm text-green-800 rounded-lg bg-green-50 dark:bg-gray-800 dark:text-green-400"
-               role="alert">
-              <span className="font-medium">Success alert!</span> Change a few things up and try submitting again.
-          </div>
-          <div
-              className="p-4 mb-4 text-sm text-yellow-800 rounded-lg bg-yellow-50 dark:bg-gray-800 dark:text-yellow-300"
-              role="alert">
-              <span className="font-medium">Warning alert!</span> Change a few things up and try submitting again.
-          </div>
-          <div className="p-4 text-sm text-gray-800 rounded-lg bg-gray-50 dark:bg-gray-800 dark:text-gray-300"
-               role="alert">
-              <span className="font-medium">Dark alert!</span> Change a few things up and try submitting again.
-          </div>
-
-
-          <button data-modal-target="default-modal" data-modal-toggle="default-modal"
-                  className="block text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
-                  type="button">
-              Toggle modal
+          <button type="button" className="btn btn-primary" data-bs-toggle="modal" data-bs-target="#exampleModal">
+              Launch demo modal
           </button>
 
-          <div id="default-modal" tabIndex="-1" aria-hidden="true"
-               className="hidden overflow-y-auto overflow-x-hidden fixed top-0 right-0 left-0 z-50 justify-center items-center w-full md:inset-0 h-[calc(100%-1rem)] max-h-full">
-              <div className="relative p-4 w-full max-w-2xl max-h-full">
-
-                  <div className="relative bg-white rounded-lg shadow dark:bg-gray-700">
-
-                      <div
-                          className="flex items-center justify-between p-4 md:p-5 border-b rounded-t dark:border-gray-600">
-                          <h3 className="text-xl font-semibold text-gray-900 dark:text-white">
-                              Terms of Service
-                          </h3>
-                          <button type="button"
-                                  className="text-gray-400 bg-transparent hover:bg-gray-200 hover:text-gray-900 rounded-lg text-sm w-8 h-8 ms-auto inline-flex justify-center items-center dark:hover:bg-gray-600 dark:hover:text-white"
-                                  data-modal-hide="default-modal">
-                              <svg className="w-3 h-3" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none"
-                                   viewBox="0 0 14 14">
-                                  <path stroke="currentColor" strokeLinecap="round" strokeLinejoin="round"
-                                        strokeWidth="2" d="m1 1 6 6m0 0 6 6M7 7l6-6M7 7l-6 6"/>
-                              </svg>
-                              <span className="sr-only">Close modal</span>
-                          </button>
+          <div className="modal fade" id="exampleModal" tabIndex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
+              <div className="modal-dialog">
+                  <div className="modal-content">
+                      <div className="modal-header">
+                          <h1 className="modal-title fs-5" id="exampleModalLabel">Modal title</h1>
+                          <button type="button" className="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
                       </div>
-
-                      <div className="p-4 md:p-5 space-y-4">
-                          <p className="text-base leading-relaxed text-gray-500 dark:text-gray-400">
-                              With less than a month to go before the European Union enacts new consumer privacy laws
-                              for its citizens, companies around the world are updating their terms of service
-                              agreements to comply.
-                          </p>
-                          <p className="text-base leading-relaxed text-gray-500 dark:text-gray-400">
-                              The European Union’s General Data Protection Regulation (G.D.P.R.) goes into effect on May
-                              25 and is meant to ensure a common set of data rights in the European Union. It requires
-                              organizations to notify users as soon as possible of high-risk data breaches that could
-                              personally affect them.
-                          </p>
+                      <div className="modal-body">
+                          ...
                       </div>
-
-                      <div
-                          className="flex items-center p-4 md:p-5 border-t border-gray-200 rounded-b dark:border-gray-600">
-                          <button data-modal-hide="default-modal" type="button"
-                                  className="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">I
-                              accept
-                          </button>
-                          <button data-modal-hide="default-modal" type="button"
-                                  className="py-2.5 px-5 ms-3 text-sm font-medium text-gray-900 focus:outline-none bg-white rounded-lg border border-gray-200 hover:bg-gray-100 hover:text-blue-700 focus:z-10 focus:ring-4 focus:ring-gray-100 dark:focus:ring-gray-700 dark:bg-gray-800 dark:text-gray-400 dark:border-gray-600 dark:hover:text-white dark:hover:bg-gray-700">Decline
-                          </button>
+                      <div className="modal-footer">
+                          <button type="button" className="btn btn-secondary" data-bs-dismiss="modal">Close</button>
+                          <button type="button" className="btn btn-primary">Save changes</button>
                       </div>
                   </div>
               </div>
           </div>
-
       </>
   )
 }
