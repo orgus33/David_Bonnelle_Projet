5bd20146ccdb36e473a5100167492f27a035d53e
Claire <claire.david3131@gmail.com>
2025-02-04 18:36:07 +0100

    Avancée comportement de base

--- src/App.jsx
+++ src/App.jsx
@@ -1,38 +1,10 @@
-import Grille from "./components/Grille.jsx";
-import {useState} from "react";
+import Jeu from "./Jeu.jsx";
 
 const App = () => {
 
-    const [isOpen, setIsOpen] = useState(false);
-
-    const togglePopover = () => {
-        setIsOpen(!isOpen);
-    };
-
   return (
       <>
-          <h1 className=" font-bold underline">
-              TODO: Faire les consignes du jeu ici et le reste (style de base de la page, footer ?, ...)
-          </h1>
-
-          <div className="relative inline-block">
-              <button
-                  onClick={togglePopover}
-                  className="bg-blue-500 text-white px-4 py-2 rounded hover:bg-blue-600"
-              >
-                  Toggle Popover
-              </button>
-              {isOpen && (
-                  <div className="absolute left-0 mt-2 w-48 bg-white border border-gray-300 rounded-lg shadow-lg">
-                      <div className="p-4 text-sm text-gray-700">
-                          <p>Voici un popover simple.</p>
-                          <p>Ajoutez plus de contenu ici !</p>
-                      </div>
-                  </div>
-              )}
-          </div>
-
-          <Grille/>
+          <Jeu/>
       </>
   )
 }
--- src/components/Grille.jsx
+++ src/components/Grille.jsx
@@ -1,7 +1,37 @@
+import {useEffect, useState} from "react";
+import Case from "./Case.jsx";
+
 function Grille({mode, data, dataUpdate}) {
+    let [hauteur, setHauteur] = useState(1);
+    let [largeur, setLargeur] = useState(1);
+    useEffect(() => {
+        switch (mode) {
+            case 1:
+                setHauteur(10);
+                setLargeur(10);
+                break;
+            case 2:
+                setHauteur(15);
+                setLargeur(15);
+                break;
+            case 3:
+                setHauteur(20);
+                setLargeur(20);
+                break;
+            default:
+                setHauteur(10);
+                setLargeur(10);
+        }
+    }, [mode]);
 
     return (
         <div>
+            <div className="grid gap-0" style={{ gridTemplateColumns: `repeat(${largeur}, 0fr)` }}>
+                {Array.from({ length: hauteur * largeur }).map((_, index) => (
+                    <Case key={index} state={0}/>
+                ))}
+            </div>
+
             <p>Grille de difficulté {mode}</p>
             <input type="range" className="form-range" id="customRange1" value={data[0]}
                    min="0" max="200"
